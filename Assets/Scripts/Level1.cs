7c412279ef2f3ce690cee7635324c7beeeded8f3
Matan Maron <38442407+matanmaron@users.noreply.github.com>
2021-05-12 00:08:11 +0300

    first level done

--- Assets/Scripts/Level1.cs
+++ Assets/Scripts/Level1.cs
@@ -1,5 +1,6 @@
 using Shecodes.Frame;
 using Shecodes.Managers;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,19 +11,28 @@ namespace Shecodes.Levels
     public class Level1 : MonoBehaviour
     {
         [SerializeField] Button ButtonRight;
+        [SerializeField] Button ButtonUp;
         [SerializeField] int MaxSteps;
+        [SerializeField] Vector2Int PlayerPosition;
+        [SerializeField] Vector2Int GoalPosition;
 
         void Start()
         {
-            GameManager.Instance.SetPlayer(-7, 3);
-            GameManager.Instance.SetGoal(0,3);
+            GameManager.Instance.SetPlayer(PlayerPosition.x, PlayerPosition.y);
+            GameManager.Instance.SetGoal(GoalPosition.x, GoalPosition.y);
             GameManager.Instance.SetSteps(MaxSteps);
             ButtonRight.onClick.AddListener(OnButtonClickRight);
+            ButtonUp.onClick.AddListener(OnButtonClickUp);
         }
 
         private void OnButtonClickRight()
         {
             GameManager.Instance.MovePlayer(Direction.Right);
+        }
+
+        private void OnButtonClickUp()
+        {
+            GameManager.Instance.MovePlayer(Direction.Up);
         }
     }
 }
